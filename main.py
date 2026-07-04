c6b273ac4406047c5c295f5419c0b0407425388d
Cristi5-47 <cr0877957@gmail.com>
2026-07-04 14:07:29 +0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,28 @@
+from aiogram import Bot, Dispatcher
+from aiogram.filters import CommandStart
+from aiogram.types import Message
+from aiogram.client.default import DefaultBotProperties
+from aiogram.enums import ParseMode
+import asyncio
+import os
+
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+
+dp = Dispatcher()
+
+@dp.message(CommandStart())
+async def start(message: Message):
+    await message.answer(
+        "👋 Assalomu alaykum!\n\n"
+        "Nakrutka Xizmat botiga xush kelibsiz!"
+    )
+
+async def main():
+    bot = Bot(
+        token=BOT_TOKEN,
+        default=DefaultBotProperties(parse_mode=ParseMode.HTML)
+    )
+    await dp.start_polling(bot)
+
+if __name__ == "__main__":
+    asyncio.run(main())
